1587b53c9ba832ffe623391cf774f10143d9fcbb
adiyounes <younesadi18@gmail.com>
2025-01-08 06:51:26 +0100

    f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mobile_app_project"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mobile_app_project"
